89887e57a901fa733efda5534223bcabe7ffe541
ikramjamiul1 <ikramjamiulspam@gmail.com>
2026-02-09 23:52:56 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+function goTo(num) {
+  document.querySelectorAll('.screen').forEach(s => {
+    s.classList.remove('active');
+  });
+
+  document.getElementById('screen-' + num).classList.add('active');
+}
 
